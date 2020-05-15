ee940424c7ba014ca182e510b9614206bc3e33db
bhandvaya <44464820+bhandavya@users.noreply.github.com>
2020-05-15 16:54:30 +0530

    msg

--- admin.php
+++ admin.php
@@ -1,65 +1,15 @@
-<?php
-    include_once "scripts/checklogin.php";
-    include_once "scripts/DB.php";
-    include_once "include/header.php";
-
-    if (!check("admin")) {
-        header('Location: logout.php');
-        exit();
-    }
-
-    $sql = "SELECT b.*, p.name AS provider_name FROM bookings AS b, providers AS p WHERE b.provider_id = p.id ORDER BY b.date DESC";
-
-    $bookings = DB::query($sql)->fetchAll(PDO::FETCH_OBJ);
-
-
-    include_once "msg/admin.php";
-?>
-<div class="container" style="margin-top: 30px; margin-bottom: 60px;">
-    <h2 class="text-center"> Bookings </h2>
-    <div class="table-responsive">
-        <table class="table">
-            <tr>
-                <th>Name</th>
-                <th>Contact</th>
-                <th>Address</th>
-                <th>Date</th>
-                <th>Payment Method</th>
-                <th>Queries</th>
-                <th>Provider Name</th>
-                <th>Action</th>
-            </tr>
-            <?php foreach ($bookings as $booking): ?>
-            <tr>
-                <td>
-                    <?= $booking->fname; ?> <?= $booking->lname; ?>
-                </td>
-                <td>
-                    <?= $booking->contact; ?>
-                </td>
-                <td>
-                    <?= $booking->adder; ?>
-                </td>
-                <td>
-                    <?= $booking->date; ?>
-                </td>
-                <td>
-                    <?= $booking->payment; ?>
-                </td>
-                <td>
-                    <?= $booking->queries; ?>
-                </td>
-                <td>
-                    <?= $booking->provider_name; ?>
-                </td>
-                <td>
-                    <a class="btn btn-danger"
-                        href="deletebooking.php?id=<?= $booking->id; ?>">Remove</a>
-                </td>
-            </tr>
-            <?php endforeach; ?>
-        </table>
+<?php if (isset($_GET['msg'])): ?>
+<div class="container" style="margin-top: 30px">
+    <?php if ($_GET['msg'] == 'success'): ?>
+    <div class="alert alert-success">
+        <h3>Success</h3>
+        <p>Booking removed successfully.</p>
     </div>
+    <?php elseif ($_GET['msg'] == 'failed'): ?>
+    <div class="alert alert-danger">
+        <h3>Failure</h3>
+        <p>Problem while removing booking! Please try again later!</p>
+    </div>
+    <?php endif; ?>
 </div>
-
-<?php include_once "include/footer.php";
+<?php endif;
--- register.php
+++ register.php
@@ -1,95 +1,24 @@
-<?php include_once "./include/header.php"; ?>
+<?php if (isset($_GET['msg'])): ?>
+<div class="container" style="margin-top: 30px">
 
-<?php
-$cities =["Hyderabad","Warangal","Nizamabad","Khammam","Karimnagar","Ramagundam","Mahabubnagar","Nalgonda","Adilabad","Suryapet","Miryalaguda","Jagtial","Vijayavada","Vyzag","Kakinada","East Godavari"];
-?>
-<?php include_once "msg/register.php"; ?>
-<div class="container" style="margin-top: 30px; max-width: 800px;margin-bottom: 60px;">
-    <div class="card">
-        <div class="card-body">
-            <div class="card-title">
-                <h3 class="text-center">Register as Service Provider</h3>
-            </div>
-            <hr>
-
-
-            <form action="scripts/register.php" method="post" enctype="multipart/form-data">
-                <div class="form-group">
-                    <label for="">Name</label>
-                    <input id="name" name="name" type="text" class="form-control" placeholder="Name" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="">Contact No.</label>
-                    <input id="contact"
-                        oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');"
-                        name="contact" type="text" class="form-control" placeholder="Contact" minlength="10"
-                        maxlength="10" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="">Address Line 1</label>
-                    <input id="adder1" name="adder1" type="text" class="form-control" placeholder="Enter Address line-1"
-                        required>
-                </div>
-
-                <div class="form-group">
-                    <label for="">Address Line 2</label>
-                    <input id="adder2" name="adder2" type="text" class="form-control" placeholder="Enter Address line-2"
-                        required>
-                </div>
-
-                <div class="form-group">
-                    <label for="">City</label>
-                    <select class="form-control" name="city" id="city">
-                        <?php foreach ($cities as $city) : ?>
-                        <option value="<?= $city ?>"> <?= $city ?>
-                        </option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="">Photo(Square Size)</label>
-                    <input id="photo" name="photo" type="file" class="form-control-file" placeholder="Select Photo 1"
-                        required>
-                </div>
-
-                <div class="form-group">
-                    <label for="">Add Description</label>
-                    <textarea name="descr" id="descr" class="form-control" cols="30" rows="5"
-                        placeholder="Tell something about you..." required></textarea>
-                </div>
-
-                <div class="form-group">
-                    <label for="">Password</label>
-                    <input id="password" name="password" type="password" class="form-control"
-                        placeholder="Enter 6 Character Password" minlength="4" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="">Profession</label>
-                    <select class="form-control" name="profession" id="profession">
-                        <option value="electrician">Electrician</option>
-                        <option value="plumber">Plumber</option>
-                        <option value="mobile">Mobile Repairer</option>
-                        <option value="nurse">Nurse</option>
-                        <option value="maid">Maid</option>
-                        <option value="carpenter">Carpenter</option>
-                        <option value="mechanic">Mechanic</option>
-                        <option value="nurse">Nurse</option>
-                        <option value="driver">Driver</option>
-                        <option value="gardener">Gardener</option>
-                        <option value="doctor">Doctor</option>
-                    </select>
-                </div>
-
-                <button style="margin-top: 30px;" class="btn btn-block btn-primary" type="submit" name="register"
-                    id="register">Register</button>
-            </form>
-
-        </div>
+    <?php if ($_GET['msg'] == 'success'): ?>
+    <div class="alert alert-success">
+        <h4>Registering</h4>
+        <p>Service provider registered.</p>
     </div>
-</div>
 
-<?php include_once "./include/footer.php";
+    <?php elseif ($_GET['msg'] == 'failed'): ?>
+    <div class="alert alert-danger">
+        <h4>Failed</h4>
+        <p>Problem while Registering! Please try again later!</p>
+    </div>
+
+    <?php elseif ($_GET['msg'] == 'file'): ?>
+    <div class="alert alert-danger">
+        <h4>Problem While Uploding Photo</h4>
+        <p>Problem while Uploding Photo! Please Try again later!</p>
+    </div>
+    <?php endif; ?>
+
+</div>
+<?php endif;
