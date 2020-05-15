5bc29189dafc527f879363336b54349455149390
bhandvaya <44464820+bhandavya@users.noreply.github.com>
2020-05-15 16:53:34 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<footer
+    style="border-top: 1px solid #fff;width: 100%; position: fixed; bottom: 0px;text-align: center;height: 40px; line-height: 40px;background: #ff8c00; ">
+    LOCAL SERVICES SEARCH SYSTEM &copy; <?= date("Y") ?>
+</footer>
+</body>
+
+</html>
